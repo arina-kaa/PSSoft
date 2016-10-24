1dc36230f616d993e3a4fe5995064471f8263ffa
arina-kaa <rfhfdfqwtdf1>
2016-10-24 12:10:00 +0300

    Добавлены тень меню и sprites на кнопки воспроизведения видео.

--- src/js/partials/main_page.js
+++ src/js/partials/main_page.js
@@ -1,8 +1,17 @@
 $(document).ready(function(){
     $(document).foundation();
     InitHeadroom();
+    InsertReversedLink();
 });
 
 function InitHeadroom(){
     $("header").headroom();
+}
+
+function InsertReversedLink(){
+    var emailE = ('info' + '@' + 'institutps' + '.ru');
+    var link = $('#reversedLink');
+
+    link.html(emailE);
+    link.attr('href', 'mailto:' + emailE);
 }
