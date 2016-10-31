c44b8925a77049d5fa236d818ad721e09cad6089
arina-kaa <rfhfdfqwtdf1>
2016-10-31 14:48:04 +0300

    Добавлена анимация на кнопки компаний.

--- src/js/partials/main_page.js
+++ src/js/partials/main_page.js
@@ -2,6 +2,7 @@ $(document).ready(function(){
     $(document).foundation();
     InitHeadroom();
     InsertReversedLink();
+    OpenCompanies();
 });
 
 function InitHeadroom(){
@@ -10,8 +11,35 @@ function InitHeadroom(){
 
 function InsertReversedLink(){
     var emailE = ('info' + '@' + 'institutps' + '.ru');
-    var link = $('#reversedLink');
+    var link = $("#reversedLink");
 
     link.html(emailE);
     link.attr('href', 'mailto:' + emailE);
+}
+
+function OpenCompanies(){
+    var visible = false;
+    $("#button-comp").click(function(){
+        if (visible){
+            $("#button-comp").text("Все IT-компании");
+            //document.getElementsByClassName('column-bottom-comp').style.display = 'none';
+            $(".column-bottom-comp").slideUp("slow");
+            //$(".column-bottom-comp").css("display", "block");
+            visible = false;
+        }
+        else {
+            $("#button-comp").text("Скрыть IT-компании");
+            //document.getElementsByClassName('column-bottom-comp').style.display = 'block';
+            $(".column-bottom-comp").slideDown("slow");
+            //$(".column-bottom-comp").css("display", "block");
+            visible = true;
+        }
+    })
+}
+
+function MobileVideo(){
+    var width = $( window ).width();
+    if (width < 640) {
+
+    }
 }
